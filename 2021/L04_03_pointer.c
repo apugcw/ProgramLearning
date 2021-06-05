9bb9e7a7be374956d8501ff141adf163faa8307d
AdmHatena <33020756+AdmHatena@users.noreply.github.com>
2021-06-06 00:46:30 +0900

    add about pointer

--- 2021/L04_03_pointer.c
+++ 2021/L04_03_pointer.c
@@ -4,10 +4,16 @@
 
 int main(void){
   
-  int number = 0; /* 変数 */
+  int number = 11; /* 変数 */
+  int *ptr; /* intのポインタを格納する変数 */
 
   printf("%d\n", number); /* 変数の値 */
   printf("%p\n", &number); /* ポインタ番号 */
+
+  ptr = &number;
+
+  printf("%p\n", ptr);
+  printf("%d\n", *ptr);
 
   return 0;
 }
