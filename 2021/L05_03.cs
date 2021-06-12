b5adc84bd00adb89f148db030950cdf7de34dce1
wata712 <83803356+wata712@users.noreply.github.com>
2021-06-13 00:39:35 +0900

    main

---
+++ 2021/L05_03.cs
@@ -0,0 +1,30 @@
+using System;
+
+ class Person //Personクラス
+    {
+        string name;
+        int age;
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            Console.WriteLine("Personクラスのコンストラクタ");
+            Console.WriteLine("名前: " + name + ", 年齢: " + age);
+        }
+    }
+    class Employee : Person // Personクラスを継承した、Employeeクラス
+    {
+        int id;
+        string department;
+        public Employee(string name, int age, int id, string department) : base(name, age)
+        {
+            this.id = id;
+            this.department = department;
+            Console.WriteLine("Employeeクラスのコンストラクタ");
+            Console.WriteLine("社員番号: " + id + ", 部署: " + department);
+        }
+        //以下のように呼び出すクラスのなかでmainを実行することも出来る
+        public static void Main(){
+            Employee empl_1 = new Employee("ichiro", 25, 123456, "system"); //インスタンスを生成
+        }
+    }
